543809f5578ce42c4c159122e00b114e85b7a491
Hurick Krügner <14249327+hurick@users.noreply.github.com>
2022-10-10 15:59:02 -0300

    chore: fixed focus reset styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,14 +1,14 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyles = createGlobalStyle`
-  :focus {
-    outline: 0;
-  }
-
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+
+    :focus {
+      outline: 0;
+    }
   }
 
   body, html, .root { height: 100%; }

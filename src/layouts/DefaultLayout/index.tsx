77eb01d028cff5e7ada724eb6932b4ac09b2cc18
Hurick Krügner <14249327+hurick@users.noreply.github.com>
2022-10-10 17:08:40 -0300

    feat(DefaultLayout): added Header to the component

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,5 +1,7 @@
 import { Outlet } from 'react-router-dom'
 
+import { Header } from '../../components/Header'
+
 import {
   DefaultLayoutContainer
 } from './styles'
@@ -7,6 +9,7 @@ import {
 export const DefaultLayout = () => {
   return (
     <DefaultLayoutContainer>
+      <Header />
       <Outlet />
     </DefaultLayoutContainer>
   )
